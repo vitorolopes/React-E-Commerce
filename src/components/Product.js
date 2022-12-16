fce663c5a845444e72325f98738cd281ac335687
vitor lopes <lopesvitor959@gmail.com>
2022-12-16 15:39:15 +0000

    Format Price

--- src/components/Product.js
+++ src/components/Product.js
@@ -15,7 +15,8 @@ const Product = ({image, name, price, id}) => {
         </div>
         <footer>
           <h5>{name}</h5>
-          <p>{price}</p>
+     {/* //! HERE */}
+          <p>{formatPrice(price)}</p>
         </footer>
     </Wrapper>
   )
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,9 @@
-export const formatPrice = () => {}
+export const formatPrice = (number) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "EUR"
+  }).format(number / 100) //! we divide by 100 because the price is in cents
+  
+}
 
 export const getUniqueValues = () => {}
